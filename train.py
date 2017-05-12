183fdfd6ed18b113fc2a2497c7647974776b1e27
Satya Kumar <vsatyakumar.official@gmail.com>
2017-05-12 04:34:38 -0400

    New Update

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from keras.models import Sequential
+from keras.layers.advanced_activations import LeakyReLU, PReLU
 from keras.optimizers import SGD, RMSprop, Adagrad
 
 from keras.layers import Dropout, Flatten, Dense, Convolution2D, LSTM, Bidirectional, Activation, TimeDistributed, GlobalAveragePooling1D, Merge, GRU
@@ -10,11 +11,12 @@ from keras.layers.normalization import BatchNormalization
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn import random_projection
 from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+
 # dimensions of our images.
 #img_width, img_height = 224, 224
 
 # fix random seed for reproducibility
-np.random.seed(1000)
+np.random.seed(1200)
 #train_data_dir = '/input/data/images/train'
 #validation_data_dir = '/input/data/images/validation'
 nb_train_samples = 14280
@@ -27,17 +29,21 @@ nb_validation_samples = 6120
 
 
 batch_size=10 #Samples per batch.
-timesteps=5 #Timesteps per sample
+timesteps=6 #Timesteps per sample
 #features_size=2048
+
+
 
 global start
 global starter
 start=1
 #Define Generator
-
+global trainbag, validationbag
 train_index_pointer = list(np.linspace(1,nb_train_samples-1, nb_train_samples, dtype=np.int64))
-
 validation_index_pointer = list(np.linspace(1,nb_validation_samples-1, nb_validation_samples, dtype=np.int64))
+trainbag=len(train_index_pointer)
+validationbag=len(validation_index_pointer)
+
 
 #Define Smooth L1 Loss
 def l1_smooth_loss(y_true, y_pred):
@@ -48,16 +54,31 @@ def l1_smooth_loss(y_true, y_pred):
 
 def generator(features, labels, batch_size, timesteps, flag=0):
 	count=0
-	print('Generator Active')
+	#print('Generator Active')
 	batch_features=np.empty((0,timesteps, features_size))
 	batch_labels=np.empty((1,0))
-
 	while True:
 
 		if flag == 0 :
-			index = train_index_pointer.pop(np.random.randint(1,len(train_index_pointer)))
+			batch_size=10
+			#trainbag=len(train_index_pointer)
+			#if trainbag<timesteps:
+				#print("Out of Novel Train Data")
+				#break
+			#else:
+			#index = train_index_pointer.pop(np.random.randint(1,trainbag))
+			index = np.random.randint(1,nb_train_samples-1, size=1, dtype=np.int64)
+			#print("Train Dataset Size", trainbag)
 		else:
-			index = validation_index_pointer.pop(np.random.randint(1,len(validation_index_pointer)))
+			batch_size=1
+			#validationbag=len(validation_index_pointer)
+			#if validationbag<timesteps:
+				#print("Out of Validation Data")
+				#break
+			#else:
+			#index = validation_index_pointer.pop(np.random.randint(1,validationbag))
+			index = np.random.randint(1,nb_validation_samples-1, size=1, dtype=np.int64)
+			#print("Validation Dataset Size", validationbag)
 
 
 		#int(index, flag)		
@@ -73,6 +94,11 @@ def generator(features, labels, batch_size, timesteps, flag=0):
 			dataX = np.vstack((dataX,x))
 			dataY= np.vstack((dataY,y))
 			
+		"""
+		if flag==0:
+			train_index_pointer=temp
+		else:
+			validation_index_pointer=temp	"""
 
 		dataY = np.mean(dataY, axis=0)
 		dataX=np.expand_dims(dataX, axis=0)
@@ -101,17 +127,22 @@ def buildmodel(summary):
 	#Define Model
 	model=Sequential()
 	model.add(BatchNormalization(input_shape=(timesteps, features_size)))
-	model.add(Bidirectional(GRU(64, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid', return_sequences=True)))
+	model.add(Bidirectional(GRU(32, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid', return_sequences=True)))
 	model.add(Bidirectional(GRU(32, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid', return_sequences=False)))
-	model.add(Dense(32, init=weight_init, activation='relu'))
+	model.add(Dense(128, init=weight_init, bias=True))
+	model.add(LeakyReLU(alpha=0.3))
 	model.add(BatchNormalization())
-	model.add(Dropout(0.2))
-	model.add(Dense(32, init=weight_init, activation='relu'))
+	model.add(Dense(64, init=weight_init, bias=True))
+	model.add(LeakyReLU(alpha=0.3))
 	model.add(BatchNormalization())
 	model.add(Dropout(0.5))
-	model.add(Dense(16, init=weight_init, activation='relu'))
+	model.add(Dense(32, init=weight_init))
+	model.add(LeakyReLU(alpha=0.3))
 	model.add(BatchNormalization())
-	model.add(Dropout(0.1))
+	model.add(Dropout(0.2))
+	model.add(Dense(16, init=weight_init))
+	model.add(LeakyReLU(alpha=0.3))
+	model.add(Dropout(0.2))
 	model.add(Dense(1, init=weight_init, activation='linear'))
 
 
@@ -120,7 +151,7 @@ def buildmodel(summary):
 	optimize=RMSprop(lr)
 	model.compile(optimizer=optimize,
 		loss=l1_smooth_loss,
-		metrics=['accuracy'])
+		metrics=['mse'])
 
 	if summary:
 		print(model.summary())
@@ -141,17 +172,16 @@ else:
 	labels.astype(float)
 
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-speeds = scaler.fit_transform(labels)
+#speeds = scaler.fit_transform(labels)
 #speeds.astype(float)
-
+speeds=labels
 y_train= speeds[0:nb_train_samples]
 y_validation= speeds[nb_train_samples:len(labels)]
 x_train=np.reshape(train_data, (nb_train_samples, -1))
 x_validation=np.reshape(validation_data, (nb_validation_samples, -1))
 
 #Reduce Dimensions
-transformer = random_projection.SparseRandomProjection(eps=0.7)
+transformer = random_projection.SparseRandomProjection(eps=0.5)
 X_train= transformer.fit_transform(x_train)
 
 features_size=X_train.shape[1]
@@ -183,22 +213,14 @@ if instance_flag==0:
 else:
 	checkpointer = ModelCheckpoint(filepath="/output/dashcam_weights.hdf5", verbose=1, save_best_only=True)
 
-training = model.fit_generator(train_generator, steps_per_epoch=10, epochs=20, verbose=1, validation_data=validation_generator, validation_steps=10, callbacks=[tensorboard, earlyStopping, checkpointer])
+training = model.fit_generator(train_generator, steps_per_epoch=20, epochs=20, verbose=1, validation_data=validation_generator, validation_steps=10, callbacks=[tensorboard, earlyStopping, checkpointer])
 
 print('Training Successful - Saving Weights...')
 
 loss_history = training.history["val_loss"]
-accuracy_history = training.history["val_acc"]
+mse_history = training.history["val_mean_squared_error"]
 
 numpy_loss_history = np.array(loss_history)
-numpy_accuracy_history = np.array(accuracy_history)
+numpy_accuracy_history = np.array(mse_history)
 np.savetxt("./loss_history.txt", numpy_loss_history, delimiter=",")
 np.savetxt("./accuracy_history.txt", numpy_accuracy_history, delimiter=",")
-
-#print('Evaluating on Validation Dataset')
-#evaluate_generator(self, validation_generator, steps=nb_validation_samples, max_q_size=10, workers=1, pickle_safe=False)
-
-#if instance_flag==0:
-#	model.save_weights('data/dashcam_weights.hdf5')
-#else:
-#	model.save_weights('/output/lstm_speed_model_weights.hdf5')
