b1763ee85e6da949110313544dc96982fd2a1960
Satya Kumar <vsatyakumar.official@gmail.com>
2017-05-15 05:37:38 -0400

    Testing New Architectures!

--- train2.py
+++ train2.py
@@ -10,16 +10,16 @@ from keras.models import model_from_json
 from keras.optimizers import RMSprop
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.core import Reshape
-from keras.layers import Dropout, Flatten, Dense, Conv2D, LSTM, Merge, Bidirectional, Activation, TimeDistributed, GRU, MaxPooling2D, Input, BatchNormalization, GlobalAveragePooling2D, SeparableConv2D, Cropping2D
+from keras.layers import Dropout, Flatten, Dense, Conv2D, LSTM, Merge, Bidirectional, Activation, TimeDistributed, GRU, MaxPooling2D, Input, BatchNormalization, GlobalAveragePooling2D, SeparableConv2D, Cropping2D, Lambda
 from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint, History
 from random import randint
 from keras import backend as K
 
 
-# fix random seed for reproducibility
+#Initializations yo.
+
 np.random.seed(2000)
-# dimensions of our images.
-img_width, img_height = 150,150
+img_width, img_height = 160,160
 nb_train_samples = 14280
 nb_validation_samples = 6120
 train_batch_size=10
@@ -31,7 +31,8 @@ channels=3 #RGB
 timesteps=3
 nb_conv_layers=1
 nb_rnn_layers = 1
-frequency=60 #Gap between frames
+frequency=60 #Gap between successive frames being fed to the model. Not to be confused with FPS (Which is 20, if you're wondering)
+fps=20
 
 global trainbag, validationbag
 train_index_pointer = list(np.linspace(1,nb_train_samples-1, nb_train_samples, dtype=np.int64))
@@ -57,7 +58,6 @@ def train():
     if instance_flag==0:
         checkpointer = ModelCheckpoint(filepath="./weights/dashcam_weights-{epoch:02d}-{val_mean_squared_error:.2f}.hdf5", verbose=1, save_best_only=True)
         tensorboard = TensorBoard(log_dir='./logs/', histogram_freq=0, write_graph=True, write_images=False)
-        history = LossHistory()
     else:
         checkpointer = ModelCheckpoint(filepath="/output/weights/dashcam_weights-{epoch:02d}-{val_mean_squared_error:.2f}.hdf5", verbose=1, save_best_only=True)
         tensorboard = TensorBoard(log_dir='/output/logs', histogram_freq=0, write_graph=True, write_images=False)
@@ -81,11 +81,11 @@ def train():
     
     model.fit_generator(train_generator, steps_per_epoch=50, epochs=20, verbose=1, validation_data=validation_generator, validation_steps=10, callbacks=[tensorboard, earlyStopping, checkpointer])
     
-        # serialize model to JSON
+    #Serialize model to JSON
     model_json = model.to_json()
     with open("./model/speed-predictor-keras-model-{val_mean_squared_error:.2f}.json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
+
     print("Saving Model to disk...")
     print('Saving History...')
     if instance_flag==0:
@@ -95,6 +95,7 @@ def train():
     else:
         np.savetxt("/ouput/logs/loss_history.txt", numpy_loss_history, delimiter=",")
     return
+    print('We are done! Hope that was a terrific training sesh... :)')
 
 def buildmodel(summary):
 
@@ -103,7 +104,7 @@ def buildmodel(summary):
     lr = 0.001
     weight_init='glorot_uniform'
     stateful=False
-    Act =LeakyReLU(alpha=7.0)
+    Act =LeakyReLU(alpha=0.3)
     #l1_gru=0.1
     #l2_gru=0.5
     #l1_dense=0.1
@@ -114,27 +115,28 @@ def buildmodel(summary):
     data= Input(shape=(None,img_width,img_height,channels))
     #Define Sequential Model
     convs= Sequential()
-    convs.add(BatchNormalization(axis=3, input_shape=(img_width,img_height, channels)))
+    convs.add(Lambda(lambda x: x/127.5 - 1.,input_shape=(img_width, img_height, channels)))
+    convs.add(BatchNormalization(axis=3))
     convs.add(Cropping2D(cropping=((60,10),(10,10))))
-    convs.add(Conv2D(8, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu'))
-    convs.add(Conv2D(16, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu'))
-    convs.add(Conv2D(32, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu'))
+    convs.add(Conv2D(8, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_1_1"))
+    convs.add(Conv2D(16, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_1_2"))
+    convs.add(Conv2D(32, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_1_3"))
     convs.add(MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
-    convs.add(Conv2D(16, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu'))
-    convs.add(Conv2D(32, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu'))
+    convs.add(Conv2D(16, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_2_1"))
+    convs.add(Conv2D(32, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_2_2"))
+    convs.add(Conv2D(64, (3, 3), kernel_initializer=weight_init, padding="same", bias=False, activation='relu',name="conv_2_3"))
     convs.add(GlobalAveragePooling2D())
     
     x=TimeDistributed(convs)(data)
-
-    x=Bidirectional(GRU(32, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid',return_sequences=False, input_shape=(timesteps,None)))(x)
+    x=Bidirectional(GRU(16, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid',return_sequences=False, input_shape=(timesteps,None,),name="gru_1"))(x)
     #x=Bidirectional(GRU(16, activation='relu', kernel_initializer=weight_init, recurrent_activation='hard_sigmoid', return_sequences=False))(x)
-    x = Dense(units=64, kernel_initializer=weight_init, bias=False)(x)
+    x = Dense(units=64, kernel_initializer=weight_init, bias=False,name="dense_1")(x)
+    x =Act(x) 
+    x=Dropout(0.2)(x)
+    x = Dense(units=16, kernel_initializer=weight_init, bias=False,name="dense_2")(x)
     x =Act(x)
     x=Dropout(0.2)(x)
-    x = Dense(units=4, kernel_initializer=weight_init, bias=False)(x)
-    x =Act(x)
-    x=Dropout(0.2)(x)
-    x=Dense(1, init=weight_init, activation='linear')(x)
+    x=Dense(1, kernel_initializer=weight_init, activation='linear',name="speed")(x)
 
     model=Model(inputs=data, outputs=x)
 
@@ -160,31 +162,27 @@ def preprocess_img(img):
     # rescale to standard size
     img = cv2.resize(img,(img_width, img_height), interpolation = cv2.INTER_AREA)
 
-    #img=np.array(img, dtype=np.uint8)
-    #-----Converting image to LAB Color model----------------------------------- 
-    lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-
-    #-----Splitting the LAB image to different channels-------------------------
-    l, a, b = cv2.split(lab)
-
-    #-----Applying CLAHE to L-channel-------------------------------------------
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    cl = clahe.apply(l)
-
-    #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
-    limg = cv2.merge((cl,a,b))
-
-    #-----Converting image from LAB Color model to RGB model--------------------
+    #NOTE-------------CLAHE hurts performance!----------------------------------
+    #Reference for the claim (tested personally) : https://arxiv.org/pdf/1606.02228v2.pdf----
+    #Global [42] and local(CLAHE [43]) histogram equalizations hurt performance as well
     
-    img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-    img= np.array(img, dtype=np.float)
-    img/= 255.0
+
+    #img=np.array(img, dtype=np.uint8)
+    #lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    #l, a, b = cv2.split(lab)
+    #-----Applying CLAHE to L-channel-------------------------------------------
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    #cl = clahe.apply(l)
+    #limg = cv2.merge((cl,a,b))
+    #img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
+    #----------------------------------------------
+    
+    img= np.array(img, dtype='float32')
     #img = np.swapaxes(img,0,1)
     img=np.expand_dims(img,axis=0)
-
     # roll color axis to axis 0
     #img = np.swapaxes(img,0,1)
-
     return img
 
 #Define Smooth L1 Loss
